46bb2da4c823cecd8ae3b3fa27728c958bb151b1
Vasyliev Hennadii <vga.kha@gmail.com>
2025-12-05 15:59:47 +0100

    Admin. Created ExperienceController.

---
+++ app/Admin/Controllers/ExperienceController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Experience;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ExperienceController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Experience controller';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Experience);
+
+        $grid->column('id', __('ID'))->sortable();
+        $grid->column('position', __('Position'))->sortable();
+        $grid->column('short_description', __('Short Description'))->sortable();
+        $grid->column('date_from', __('Date From'))->sortable()->date();
+        $grid->column('date_to', __('Date To'))->sortable()->date();
+        $grid->column('company_name', __('Company'))->sortable();
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed   $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Experience::findOrFail($id));
+
+        $show->field('id', __('ID'));
+        $show->field('position', __('Position'));
+        $show->field('short_description', __('Short Description'));
+        $show->field('date_from', __('Date From'));
+        $show->field('date_to', __('Date To'));
+        $show->field('company_name', __('Company'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Experience);
+
+        $form->display('id', __('ID'));
+        $form->text('position', __('Position'));
+        $form->textarea('short_description', __('Short Description'));
+        $form->date('date_from', __('Date From'));
+        $form->date('date_to', __('Date To'));
+        $form->text('company_name', __('Company'));
+
+        return $form;
+    }
+}
