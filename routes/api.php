1fee2905f5559d25c5d2d89509428c87f0d24aa7
Vasyliev Hennadii <vga.kha@gmail.com>
2026-01-03 15:26:07 +0100

    Setting the route for ProjectController in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 123;
-});
+Route::apiResource('/projects', ProjectController::class);
